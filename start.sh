da44a0fabd6f3dda01aa0e64e1c441cc05d90183
shijianhang <3076679995@qq.com>
2021-11-26 18:07:46 +0800

    完善启动脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-python3 manage.py runserver 0.0.0.0:8080
+echo '1 启动web'
+python3 manage.py runserver 0.0.0.0:8080 &
+
+if [ "$1" == -a ]
+then
+  echo '2 启动celery'
+  echo '2.1 启动worker'
+  python3 manage.py celery -A HttpRunnerManager worker --loglevel=info
+  echo '2.2 启动定时任务监听器'
+  python3 manage.py celery beat --loglevel=info
+  echo '2.3 启动任务监控后台'
+  celery flower
+fi
