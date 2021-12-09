e7e65c9edc49029114de1491da305a8ae47113e8
shijianhang <3076679995@qq.com>
2021-12-09 11:26:56 +0800

    完善启动命令

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ if [ "$1" == -a ]
 then
   echo '2 启动celery'
   echo '2.1 启动worker'
-  python3 manage.py celery -A HttpRunnerManager worker --loglevel=info
+  python3 manage.py celery -A HttpRunnerManager worker --loglevel=info &
   echo '2.2 启动定时任务监听器'
-  python3 manage.py celery beat --loglevel=info
-  echo '2.3 启动任务监控后台'
-  celery flower
+  python3 manage.py celery beat --loglevel=info &
+  echo '2.3 启动任务监控后台' 
+  celery flower --address=0.0.0.0 --port=5555 --broker=redis://192.168.0.184/6 &
 fi
